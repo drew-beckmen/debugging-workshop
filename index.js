01385e8f74722c9c55ad1d3e3aae743ba5c1e140
Drew Beckmen <drew.beckmen@yale.edu>
2020-07-13 10:20:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
   const jokeList = document.getElementById('joke-list')
-  const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
-  let joke;
+  const username = document.getElementById('name-input') 
+  let joke; 
 
   function fetchJoke(){
-    fetch('https://icanhazdadjoke.com/', {
+    return fetch('https://icanhazdadjoke.com/', {
       headers: {
         "Accept": "application/json"
       }
     })
-    .then(res => res.json())
-    .then(jokeData => joke = jokeData.joke)
+    .then(resp => resp.json())
+    //All the adding to the DOM needs to happen in the asynchronous function 
+    // Otherwise, event listener callback will continue executing with a pending Promise
+    .then(data => {
+      joke = data.joke
+      const newJokeLi = document.createElement('li')
+      newJokeLi.innerHTML = `<span class="username">${username.value} says:</span> ${joke}`
+      jokeList.appendChild(newJokeLi)
+      form.reset()
+    })
   }
 
   form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    if(username.value === "") return;
+    fetchJoke() 
 
-    if(username === "") return;
-    fetchJoke()
-    newJokeLi.innerHTML = `
-    <span class="username">${username} says:</span> ${joke}
-    `
-    jokeList.appendChild(newJokeLi)
+    //You CANNOT reset the form here --> it will run before fetchJoke() runs
+    // as a result, username field would be completely wiped out. 
   })
 })
